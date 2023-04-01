6cc5a0e60ee947349bc3860eb0d7548c98852fd5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-01 18:25:37 +0530

    Update anime.py

--- MukeshRobot/modules/anime.py
+++ MukeshRobot/modules/anime.py
@@ -139,9 +139,9 @@ dispatcher.add_handler(QUOTE_CHANGE)
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 dispatcher.add_handler(QUOTES_HANDLER)
 
-__mod_name__ = "⍟ ǫᴜᴏᴛᴇs ⍟"
+__mod_name__ = "✨ǫᴜᴏᴛᴇs✨"
 __help__ = """
- ©️ [ᴍᴜᴋᴇsʜ] (f"://user_id={OWNER_ID}"))
+ ©️ ʙʏ [VIP BOY](https://t.me/the_vip_boy)
 /quote :- ᴡʀɪᴛᴇ ǫᴜᴏᴛᴇs
 /animequotes :- ᴡʀɪᴛᴇ ᴀɴɪᴍᴇǫᴜᴏᴛᴇs
  """
