635f32fee6d5f04257174763982d4b5251beeb1a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-01 22:05:01 +0530

    Update dbcleanup.py

--- MukeshRobot/modules/dbcleanup.py
+++ MukeshRobot/modules/dbcleanup.py
@@ -147,5 +147,5 @@ BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern="db_.*")
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ ="⍟ ᴅᴀᴛᴀʙᴀsᴇ ⍟"
+__mod_name__ ="🍄ᴅᴀᴛᴀʙᴀsᴇ🍄"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
