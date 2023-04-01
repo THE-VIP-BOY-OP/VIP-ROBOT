b2c98f395827d78df1faa74ec3391a07d3358231
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-01 20:13:11 +0530

    Update blacklist.py

--- MukeshRobot/modules/blacklist.py
+++ MukeshRobot/modules/blacklist.py
@@ -449,7 +449,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "⍟ Bʟᴀᴄᴋʟɪsᴛ ⍟"
+__mod_name__ = "❗Bʟᴀᴄᴋʟɪsᴛ❗"
 
 __help__ = """
 
@@ -463,6 +463,8 @@ Admin only:
  ❍ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
  ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
  ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
+
+☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
 """
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
