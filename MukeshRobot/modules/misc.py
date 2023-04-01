a90eaa05e22b4578106f58cba91917da0c9327b5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 00:11:37 +0530

    Update misc.py

--- MukeshRobot/modules/misc.py
+++ MukeshRobot/modules/misc.py
@@ -100,6 +100,7 @@ __help__ = """
  `/ᴄᴀsʜ 1 ᴜsᴅ ɪɴʀ`
 ᴏᴜᴛᴘᴜᴛ: `1.0 ᴜsᴅ = 75.505 ɪɴʀ`
 
+☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
 """
 
 ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
@@ -108,7 +109,7 @@ MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 
-__mod_name__ = "⍟ Exᴛʀᴀs ⍟"
+__mod_name__ = "🤞Exᴛʀᴀs🤞"
 __command_list__ = ["id", "echo"]
 __handlers__ = [
     ECHO_HANDLER,
