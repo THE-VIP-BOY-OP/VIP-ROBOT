f48a0e34e7f5d09be3a84045387ba9e0638bf5b8
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 11:11:06 +0530

    Update afk.py

--- MukeshRobot/modules/afk.py
+++ MukeshRobot/modules/afk.py
@@ -164,7 +164,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "🥀Aꜰᴋ🥀"
+__mod_name__ = "⍟ Aꜰᴋ ⍟"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
