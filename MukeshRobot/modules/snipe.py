0cfb2408327e72d3e71c9ad1099c293ffe336036
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 22:47:41 +0530

    Update snipe.py

--- MukeshRobot/modules/snipe.py
+++ MukeshRobot/modules/snipe.py
@@ -34,7 +34,7 @@ __help__ = """
 ☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
 """
 
-__mod_name__ = "🌺sɴɪᴘᴇ🌺"
+__mod_name__ = "⚡sɴɪᴘᴇ⚡"
 
 SNIPE_HANDLER = CommandHandler(
     "snipe", snipe, pass_args=True, filters=CustomFilters.dev_filter
