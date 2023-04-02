5508006c822f3e31794d0e29b317f74b221bb338
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 19:43:42 +0530

    Update gitinfo.py

--- MukeshRobot/modules/gitinfo.py
+++ MukeshRobot/modules/gitinfo.py
@@ -45,7 +45,7 @@ async def github(_, message):
     await message.reply_photo(photo=avatar_url, caption=caption)
 
 
-__mod_name__ = "🖤Gɪᴛʜᴜʙ🖤"
+__mod_name__ = "📍Gɪᴛʜᴜʙ📍"
 
 __help__ = """
 ɪ ᴡɪʟʟ ɢɪᴠᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ɢɪᴛʜᴜʙ ᴘʀᴏғɪʟᴇ 
