6e3d9f9aec691d84fe5dfb2b81430267bba989a4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 19:29:28 +0530

    Update eval.py

--- MukeshRobot/modules/eval.py
+++ MukeshRobot/modules/eval.py
@@ -134,7 +134,7 @@ dispatcher.add_handler(EVAL_HANDLER)
 dispatcher.add_handler(EXEC_HANDLER)
 dispatcher.add_handler(CLEAR_HANDLER)
 
-__mod_name__ = "🌺ᴇᴠᴀʟ🌺"
+__mod_name__ = "♨️ᴇᴠᴀʟ♨️"
 __help__ = """
 ★ᴏᴡɴᴇʀ ᴄᴍᴅ ★
 ★ /eval :- to evaluate simple code
