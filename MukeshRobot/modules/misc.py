bc9c95766e1a666dafb95fbcc123f221618accc6
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 19:59:00 +0530

    Update misc.py

--- MukeshRobot/modules/misc.py
+++ MukeshRobot/modules/misc.py
@@ -109,7 +109,7 @@ MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 
-__mod_name__ = "🤞Exᴛʀᴀs🤞"
+__mod_name__ = "📍Exᴛʀᴀs📍"
 __command_list__ = ["id", "echo"]
 __handlers__ = [
     ECHO_HANDLER,
