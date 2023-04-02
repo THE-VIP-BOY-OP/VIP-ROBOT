bcf32f06cbff2e42f02b50803fae24cf2cb85231
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 19:41:20 +0530

    Update fun.py

--- MukeshRobot/modules/fun.py
+++ MukeshRobot/modules/fun.py
@@ -374,7 +374,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "🤹Fᴜɴ🤹"
+__mod_name__ = "📍Fᴜɴ📍"
 __command_list__ = [
     "runs",
     "slap",
