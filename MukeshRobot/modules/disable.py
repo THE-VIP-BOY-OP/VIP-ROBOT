580962f8bb06dc8f9738c9ca4ff7b932cd7e8cf2
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 19:23:09 +0530

    Update disable.py

--- MukeshRobot/modules/disable.py
+++ MukeshRobot/modules/disable.py
@@ -352,7 +352,7 @@ if is_module_loaded(FILENAME):
 ☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
     """
 
-    __mod_name__ = " 🐉Dɪsᴀʙʟᴇ🐉"
+    __mod_name__ = " ♨️Dɪsᴀʙʟᴇ♨️"
 
 else:
     DisableAbleCommandHandler = CommandHandler
