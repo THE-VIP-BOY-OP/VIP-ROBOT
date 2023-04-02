d4c5af710de1567d6daa6f94032e0acdb4880489
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 20:05:40 +0530

    Update muting.py

--- MukeshRobot/modules/muting.py
+++ MukeshRobot/modules/muting.py
@@ -257,5 +257,5 @@ dispatcher.add_handler(MUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 
-__mod_name__ = "🤫Mᴜᴛᴇ​🤫"
+__mod_name__ = "📍Mᴜᴛᴇ📍"
 __handlers__ = [MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]
