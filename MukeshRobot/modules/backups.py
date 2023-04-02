c68d4d117e71c9e8b3a334834d25ac224f548419
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 18:08:22 +0530

    Update backups.py

--- MukeshRobot/modules/backups.py
+++ MukeshRobot/modules/backups.py
@@ -368,7 +368,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "💕Bᴀᴄᴋᴜᴘ💕"
+__mod_name__ = "♨️Bᴀᴄᴋᴜᴘ♨️"
 
 __help__ = """
 *ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘ ᴏᴡɴᴇʀ:*
