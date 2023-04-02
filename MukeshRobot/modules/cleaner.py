fa89f2b7f2bae3ca0d24872ee674ccc7f95f4258
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 12:50:51 +0530

    Update cleaner.py

--- MukeshRobot/modules/cleaner.py
+++ MukeshRobot/modules/cleaner.py
@@ -253,7 +253,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "🍜ᴄʟᴇᴀɴ🍜"
+__mod_name__ = "⍟ ᴄʟᴇᴀɴ ⍟"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
