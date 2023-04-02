e014b32896cbf494c0c944ab41b1c267f5c93d7f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 18:13:55 +0530

    Update blacklist_stickers.py

--- MukeshRobot/modules/blacklist_stickers.py
+++ MukeshRobot/modules/blacklist_stickers.py
@@ -509,7 +509,7 @@ __help__ = """
 ☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
 """
 
-__mod_name__ = "🧁sᴛɪᴄᴋᴇʀ🧁"
+__mod_name__ = "♨️sᴛɪᴄᴋᴇʀ♨️"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
