350edc4c09343364177725c0a7e3c750eaf0dc49
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 19:44:58 +0530

    Update global_bans.py

--- MukeshRobot/modules/global_bans.py
+++ MukeshRobot/modules/global_bans.py
@@ -531,7 +531,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "👽AA-Sᴘᴀᴍ👽"
+__mod_name__ = "📍AA-Sᴘᴀᴍ📍"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
