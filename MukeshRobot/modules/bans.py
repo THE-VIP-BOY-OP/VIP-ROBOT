143f028842826a8daab0b6f001bc15f4e158c5db
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-02 18:10:10 +0530

    Update bans.py

--- MukeshRobot/modules/bans.py
+++ MukeshRobot/modules/bans.py
@@ -427,7 +427,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
 
-__mod_name__ = "📍ʙᴀɴ📍"
+__mod_name__ = "♨️ʙᴀɴ♨️"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
