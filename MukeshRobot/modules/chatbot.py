16228dd0ddbdc24b35f9cdec9fb36d48bc65d539
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-31 21:08:54 +0530

    Update chatbot.py

--- MukeshRobot/modules/chatbot.py
+++ MukeshRobot/modules/chatbot.py
@@ -19,6 +19,7 @@ from telegram.ext import (
     CommandHandler,
     Filters,
     MessageHandler,
+    run_async,
 )
 from telegram.utils.helpers import mention_html
 
@@ -28,6 +29,7 @@ from MukeshRobot.modules.helper_funcs.chat_status import user_admin, user_admin_
 from MukeshRobot.modules.log_channel import gloggable
 
 
+@run_async
 @user_admin_no_reply
 @gloggable
 def fallenrm(update: Update, context: CallbackContext) -> str:
@@ -56,6 +58,7 @@ def fallenrm(update: Update, context: CallbackContext) -> str:
     return ""
 
 
+@run_async
 @user_admin_no_reply
 @gloggable
 def fallenadd(update: Update, context: CallbackContext) -> str:
@@ -84,6 +87,7 @@ def fallenadd(update: Update, context: CallbackContext) -> str:
     return ""
 
 
+@run_async
 @user_admin
 @gloggable
 def fallen(update: Update, context: CallbackContext):
@@ -108,7 +112,7 @@ def fallen_message(context: CallbackContext, message):
     reply_message = message.reply_to_message
     if message.text.lower() == "fallen":
         return True
-    elif BOT_USERNAME in message.text:
+    elif BOT_USERNAME in message.text.upper():
         return True
     elif reply_message:
         if reply_message.from_user.id == BOT_ID:
@@ -129,9 +133,8 @@ def chatbot(update: Update, context: CallbackContext):
         if not fallen_message(context, message):
             return
         bot.send_chat_action(chat_id, action="typing")
-        request = requests.get(
-            f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/Anonymous/message={message.text}"
-        )
+        url = f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/Anonymous/message={message.text}"
+        request = requests.get(url)
         results = json.loads(request.text)
         sleep(0.5)
         message.reply_text(results["reply"])
@@ -140,7 +143,7 @@ def chatbot(update: Update, context: CallbackContext):
 __help__ = f"""
 *{BOT_NAME} ʜᴀs ᴀɴ ᴄʜᴀᴛʙᴏᴛ ᴡʜɪᴄʜ  ᴘʀᴏᴠɪᴅᴇs ʏᴏᴜ ᴀ sᴇᴇᴍɪɴɢʟᴇss ᴄʜᴀᴛᴛɪɴɢ ᴇxᴘᴇʀɪᴇɴᴄᴇ :*
 
-»  /ᴄʜᴀᴛʙᴏᴛ *:* sʜᴏᴡs ᴄʜᴀᴛʙᴏᴛ ᴄᴏɴᴛʀᴏʟ ᴘᴀɴᴇʟ
+ »  /ᴄʜᴀᴛʙᴏᴛ *:* sʜᴏᴡs ᴄʜᴀᴛʙᴏᴛ ᴄᴏɴᴛʀᴏʟ ᴘᴀɴᴇʟ
 
 ☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
 """
@@ -148,14 +151,13 @@ __help__ = f"""
 __mod_name__ = "♨️Cʜᴀᴛʙᴏᴛ♨️"
 
 
-CHATBOTK_HANDLER = CommandHandler("chatbot", fallen, run_async=True)
-ADD_CHAT_HANDLER = CallbackQueryHandler(fallenadd, pattern=r"add_chat", run_async=True)
-RM_CHAT_HANDLER = CallbackQueryHandler(fallenrm, pattern=r"rm_chat", run_async=True)
+CHATBOTK_HANDLER = CommandHandler("chatbot", fallen)
+ADD_CHAT_HANDLER = CallbackQueryHandler(fallenadd, pattern=r"add_chat")
+RM_CHAT_HANDLER = CallbackQueryHandler(fallenrm, pattern=r"rm_chat")
 CHATBOT_HANDLER = MessageHandler(
     Filters.text
     & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!") & ~Filters.regex(r"^\/")),
     chatbot,
-    run_async=True,
 )
 
 dispatcher.add_handler(ADD_CHAT_HANDLER)
@@ -168,4 +170,4 @@ __handlers__ = [
     CHATBOTK_HANDLER,
     RM_CHAT_HANDLER,
     CHATBOT_HANDLER,
-]
+             ]
